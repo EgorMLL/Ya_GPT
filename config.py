2c90260cd28d80fafba5bf7778ee3761cfd75cdc
EgorMLL <151934967+EgorMLL@users.noreply.github.com>
2024-03-26 17:00:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-GPT_LOCAL_URL = 'http://localhost:1234/v1/chat/completions'
 
-HEADERS = {"Content-Type": "application/json"}
-
-TOKEN = "7018987748:AAHfhcviuGLbZRtbThx_55BESYJkRk25D-E"
-
-MAX_TOKENS: int = 200
