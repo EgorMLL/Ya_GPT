7418748180844ac4932155c6b3252f31c28865ab
EgorMLL <ymlechkov@mail.ru>
2024-03-30 20:05:27 +0300

    config

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+GPT_LOCAL_URL = 'http://localhost:1234/v1/chat/completions'
 
+HEADERS = {"Content-Type": "application/json"}
+
+TOKEN = "7018987748:AAHfhcviuGLbZRtbThx_55BESYJkRk25D-E"
+
+folder_id = "b1gd9cphe9fc3et8kkvm"
+
+token = "t1.9euelZqclIqOzZaNm4-MmpGPi8_Ozu3rnpWalZvJyZbPysmVyZ3KjprMzInl8_cgDF9P-e8VV0Zg_N3z92A6XE_57xVXRmD8zef1656VmouSysecx8eZkcrKzJWWkc7N7_zF656VmouSysecx8eZkcrKzJWWkc7NveuelZqMnJKUz5yXlpiZl5mJyY-Wz7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.aSAPhJf3J6Dw5i3JSxhRjKKIWhrf9lFui33SXt0wtF2VlPW1beJxrJJnzCYj3-bb_Hkeot6UuL4SEOo4JBuaAQ"
+
+MAX_TOKENS = 500
